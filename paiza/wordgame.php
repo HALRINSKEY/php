10062574571010141d58afb13f35dc6c6d346a3d
HALRINSKEY <hal4554yoko@gmail.com>
2023-10-03 00:48:47 +0900

    Update wordgame.php

--- paiza/wordgame.php
+++ paiza/wordgame.php
@@ -1,6 +1,6 @@
 <?php
 
-$info = explode(' ',trim(fgets(STDIN)));
+$info = explode(' ',trim(fgets(STDIN))); #0:人数N　1:単語の数K　2:しりとりの回数M
 
 $word_list = [];
 for($k = 0; $k < $info[1]; $k++){
@@ -8,13 +8,36 @@ for($k = 0; $k < $info[1]; $k++){
 }
 
 $used_word = [];
+$ban_list = [];
+$before_word = 0;
 $player = 1;
 for($m = 0; $m < $info[2]; $m++){
     $word = trim(fgets(STDIN));
-    if (in_array($word,$used_word)){
-        
+
+    
+    if ($before_word == 0){#初回
+        if ( in_array($word,$used_word) or !(in_array($word,$word_list)) ){#使用済みチェック #単語リストチェック
+            $ban_list[] = $player;
+            continue;
+        }
+    
+    }else{
+        if (substr($before_word, -1) != substr($word, -1)){#語尾チェック
+            $ban_list[] = $player;
+            $before_word = 0;
+            continue;
+        }
+
+        if ( in_array($word,$used_word) or !(in_array($word,$word_list)) ){#使用済みチェック #単語リストチェック
+            $ban_list[] = $player;
+            $before_word = 0;
+            continue;
+        }
     }
+    
+    $before_word = $word;
     $used_word = $word;
+
 }
 
 
